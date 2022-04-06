48b83024af8f2958edbba9086488cc3b90131e98
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-04-06 20:25:53 -0300

    C3.6
    Mudando alguns atributos da classe reserva

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,104 +1,50 @@
 package model;
 
-/** 
-	 * @return Apresenta as variaveis da reserva
-	 */
+/**
+ * @return Apresenta as variaveis da reserva
+ */
 public class Reserva {
-	private int periodo;
+
 	private String checkin;
 	private String checkout;
-	private int codigo;
+	private Usuario usuario;
+	private Imovel imovel;
 
-	public Reserva(int per, String cin, String cou, int cod) {
-		this.periodo = per;
+	public Reserva(Usuario u, String cin, String cou, Imovel i) {
+
 		this.checkin = cin;
 		this.checkout = cou;
-		this.codigo = cod;
-	}
-	
-	
-	/** 
-	 * @return Espaco para trazer as funcoes da reserva
-	 */
-	public String toString() {
-		return "A reserva do c�digo " + codigo + " est� reservada no per�odo de " +periodo + " dias. ";
-				
-	}
-	
-	public void cadastrarReserva() {
+		this.usuario = u;
+		this.imovel = i;
 
 	}
 
-	public void alterarReserva() {
-
-	}
-
-	public void cancelarReserva() {
-
-	}
-
-	
-	/** 
-	 * @return funcao para saber o que e o periodo
-	 */
-	public int getPeriodo() {
-		return periodo;
-	}
-
-	
-	/** 
-	 * @param funcao para definir o que e periodo
-	 */
-	public void setPeriodo(int periodo) {
-		this.periodo = periodo;
-	}
-
-	
-	/** 
+	/**
 	 * @return funcao para saber o que e checkin
 	 */
 	public String getCheckin() {
 		return checkin;
 	}
 
-	
-	/** 
+	/**
 	 * @param funcao para definir o Checkin
 	 */
 	public void setCheckin(String checkin) {
 		this.checkin = checkin;
 	}
 
-	
-	/** 
+	/**
 	 * @return funcao para saber o que e checkout
 	 */
 	public String getCheckout() {
 		return checkout;
 	}
 
-	
-	/** 
+	/**
 	 * @param funcao para definir o checkout
 	 */
 	public void setCheckout(String checkout) {
 		this.checkout = checkout;
-	}
-
-	
-	/** 
-	 * @return funcao para saber se tem codigo
-	 */
-	public int getCodigo() {
-		return codigo;
-	}
-
-	
-	/** 
-	 * @param funcao para definir o codigo
-	 */
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
 	}
 
 }
