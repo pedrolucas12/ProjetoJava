0193d852cdf663e888a10b641628edb6065dbe9a
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-04-06 20:51:36 -0300

    C3.9
    Começo da criação da tela de cadastro imóveis

--- src/view/TelaCadastroImovel.java
+++ src/view/TelaCadastroImovel.java
@@ -9,19 +9,72 @@ public class TelaCadastroImovel {
 	private static JFrame janela = new JFrame();
 	private static JButton salvar = new JButton("Salvar Reserva");
 
-	private static JLabel checkin = new JLabel(" Data Check-in:");
-	private static JLabel checkout = new JLabel("Data Check-out:");
-	private static JLabel usuario = new JLabel("Usuario:");
-	private JTextField tfCheckin = new JTextField();
-	private JTextField tfCheckout = new JTextField();
-	private JTextField tfUsuario = new JTextField();
+	private static JLabel qtdH = new JLabel("Quantidade de hospedes");
+	private static JLabel qtdB = new JLabel("Quantidade de banheiros");
+	private static JLabel qtdQ = new JLabel("Quantidade de quartos");
+	private static JLabel qtdC = new JLabel("Quantidade de camas");
+	private static JLabel end = new JLabel("Endereco");
+	private static JLabel nome = new JLabel("Nome");
+	private static JLabel val = new JLabel("Valor diaria");
+	private static JLabel ele = new JLabel("Possui elevador?");
+	private static JLabel var = new JLabel("Possui varanda?");
+	private static JLabel con = new JLabel("Possui condominio?");
+	private JTextField tfH= new JTextField();
+	private JTextField tfB = new JTextField();
+	private JTextField tfQ = new JTextField();
+	private JTextField tfC = new JTextField();
+	private JTextField tfEnd = new JTextField();
+	private JTextField tfNome = new JTextField();
+	private JTextField tfVal = new JTextField();
+	private JTextField tfEle = new JTextField();
+	private JTextField tfCon = new JTextField();
+	private JTextField tfVar = new JTextField();
 
 	public void CadastroCasa() {
-
+		
+		
 	}
 
 	public void CadastroApartamento() {
 
+		qtdH.setBounds(10, 50, 100, 20);
+		h.setBounds(10, 80, 100, 20);
+		qtdB.setBounds(10, 110, 100, 20);
+		b.setBounds(10, 140, 100, 20);
+		end.setBounds(10, 170, 100, 20);
+
+		tfNome.setBounds(70, 50, 100, 20);
+		tfCpf.setBounds(70, 80, 100, 20);
+		tfEmail.setBounds(70, 110, 100, 20);
+		tfEndereco.setBounds(70, 170, 100, 20);
+		tfTelefone.setBounds(70, 140, 100, 20);
+
+		numUsersCadastrados.setBounds(10, 230, 200, 20);
+		salvar.setBounds(70, 200, 90, 30);
+		excluir.setBounds(230, 320, 90, 30);
+		alterar.setBounds(350, 320, 90, 30);
+
+		janela.setSize(500, 400);
+		janela.setVisible(true);
+		janela.add(tfNome);
+		janela.add(tfCpf);
+		janela.add(tfEmail);
+		janela.add(t);
+		janela.add(c);
+		janela.add(e);
+		janela.add(n);
+		janela.add(tel);
+		janela.add(end);
+		janela.add(numUsersCadastrados);
+		janela.add(tfTelefone);
+		janela.add(tfEndereco);
+
+		janela.add(salvar);
+		janela.add(excluir);
+		janela.add(alterar);
+
+		janela.setLayout(null);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
