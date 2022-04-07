4fe527d07b9b4c782fe35c2c7b17e893c90b40ef
PEDRO LUCAS SANTANA <80850842+pedrolucas12@users.noreply.github.com>
2022-04-06 21:10:48 -0300

    C4.2

--- src/view/TelaCadastroImovel.java
+++ src/view/TelaCadastroImovel.java
@@ -13,22 +13,22 @@ public class TelaCadastroImovel {
 	private static JLabel qtdB = new JLabel("Quantidade de banheiros");
 	private static JLabel qtdQ = new JLabel("Quantidade de quartos");
 	private static JLabel qtdC = new JLabel("Quantidade de camas");
-	private static JLabel end = new JLabel("Endereco");
+	/*private static JLabel end = new JLabel("Endereco");
 	private static JLabel nome = new JLabel("Nome");
 	private static JLabel val = new JLabel("Valor diaria");
 	private static JLabel ele = new JLabel("Possui elevador?");
 	private static JLabel var = new JLabel("Possui varanda?");
-	private static JLabel con = new JLabel("Possui condominio?");
+	private static JLabel con = new JLabel("Possui condominio?");*/
 	private JTextField tfH= new JTextField();
 	private JTextField tfB = new JTextField();
 	private JTextField tfQ = new JTextField();
 	private JTextField tfC = new JTextField();
-	private JTextField tfEnd = new JTextField();
+	/*private JTextField tfEnd = new JTextField();
 	private JTextField tfNome = new JTextField();
 	private JTextField tfVal = new JTextField();
 	private JTextField tfEle = new JTextField();
 	private JTextField tfCon = new JTextField();
-	private JTextField tfVar = new JTextField();
+	private JTextField tfVar = new JTextField();*/
 
 	public void CadastroCasa() {
 		
@@ -43,18 +43,22 @@ public class TelaCadastroImovel {
 		tfB.setBounds(10, 140, 100, 20);
 		qtdQ.setBounds(10, 170, 100, 20);
 		tfQ.setBounds(70, 50, 100, 20);
-		tfCpf.setBounds(70, 80, 100, 20);
-		tfEmail.setBounds(70, 110, 100, 20);
-		tfEndereco.setBounds(70, 170, 100, 20);
+		qtdC.setBounds(70, 80, 100, 20);
+		tfC.setBounds(70, 110, 100, 20);
+		/*tfEndereco.setBounds(70, 170, 100, 20);
 		tfTelefone.setBounds(70, 140, 100, 20);
-		numUsersCadastrados.setBounds(10, 230, 200, 20);
+		numUsersCadastrados.setBounds(10, 230, 200, 20);*/
 		salvar.setBounds(70, 200, 90, 30);
-		excluir.setBounds(230, 320, 90, 30);
-		alterar.setBounds(350, 320, 90, 30);
+		/*excluir.setBounds(230, 320, 90, 30);
+		alterar.setBounds(350, 320, 90, 30);*/
 
 		janela.setSize(500, 400);
 		janela.setVisible(true);
 		janela.add(qtdH);
+		janela.add(qtdB);
+		janela.add(qtdQ);
+		janela.add(qtdC);
+		/*janela.add();
 		janela.add();
 		janela.add();
 		janela.add();
@@ -64,11 +68,7 @@ public class TelaCadastroImovel {
 		janela.add();
 		janela.add();
 		janela.add();
-		janela.add();
-		janela.add();
-		janela.add();
-		janela.add();
-		janela.add();
+		janela.add();*/
 
 		janela.setLayout(null);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
